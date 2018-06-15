58dbbde2215ff52db50614ddca425181f7ae2f30
Chad Collins <chad.c.collins@gmail.com>
2018-06-14 22:58:48 -0700

    close, but definitely an infinite loop somewhere

--- leetcode/src/minimum-window-substring.py
+++ leetcode/src/minimum-window-substring.py
@@ -20,15 +20,24 @@ class Solution(object):
         expected = [0 for i in range(26)]
         found = [0 for i in range(26)]
 
+        # this will be useful
+        A = ord('A')
+
         # count the characters in the input and store the counts in the array
-        for char in t:
-            expected[ord(char) - ord('A')] += 1
+        for c in t:
+            char = ord(c) - A 
+            expected[char] += 1
         
         i, count = 0, 0
 
+        # this will track the start of a window within which we have matched all characters
+        window_start = 0
+        min_window_len = -1
+        min_window_start = -1
+        # traverse s
         while i < len(s):
             # record the character found in s
-            char = ord(s[i]) - ord('A')
+            char = ord(s[i]) - A
             found[char] += 1
 
             # if the count of the character at s[i] <= the count of the same character in the input string, increment count
@@ -37,6 +46,30 @@ class Solution(object):
 
             # if count == len(t) we have consumed every character
             if count == len(t):
+                # try to find the window
+                wchar = ord(s[window_start]) - A
+                while expected[wchar] == 0 or found[wchar] > expected[wchar]:
+                    # consume the character
+                    found[wchar] -= 1
+                    # move the start forward
+                    window_start += 1
+
+                window_len = i - window_start + 1
+                # if we havent yet found a window, or the one we just found is smaller, save it
+                if min_window_len < 0 or min_window_len > window_len:
+                    min_window_len = window_len 
+                    min_window_start = window_start
                 
             # move forward in s
             i += 1
+
+        # if we didn't find anything, return empty
+        if min_window_len == -1:
+            return ""
+
+        return s[min_window_start:min_window_start + min_window_len]
+        
+
+
+if __name__ == "__main__":
+    print (Solution().minWindow("ADOBECODEBANC", "ABC"))
